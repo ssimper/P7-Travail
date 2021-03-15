812783fde69cc69b9ab125023dd9eb10de30b9e1
ssimper <steph.simper@gmail.com>
2021-03-15 23:47:30 +0100

    Ajout du JSON et des tests

--- parser/program/parser01.py
+++ parser/program/parser01.py
@@ -1,8 +1,14 @@
 import unidecode
 import string
+import json
 
 class Parser:
-
+	"""Classe chargée de passer en minuscule, retirer
+	- les accents
+	- l'accentuation
+	- la ponctuation
+	convertir la question en liste de mots et enfin de
+	nettoyer cette liste de ses stop-words"""
 
 	def __init__(self):
 		
@@ -13,18 +19,21 @@ class Parser:
 
 
 	def lowerize(self):
+		# On passe en minuscule
 		result = self.question.lower()
 		self.result = result
 		return result
 
 
 	def decodize(self):
+		# On retire l'accentuation
 		result2 = unidecode.unidecode(self.result)
 		self.result2 = result2
 		return result2
 
 
 	def remove_ponc(self):
+		# On retire la ponctuation
 		result3 = self.result2.translate(
 			str.maketrans('', '', string.punctuation)
 			)
@@ -32,9 +41,21 @@ class Parser:
 		return result3
 
 	def to_list(self):
+		# On place les mots dans une liste
 		result4 = self.result3.split(' ')
 		self.result4 = result4
 		return result4
+
+
+	def check_stop_words(self):
+		# On nettoie la liste des stop-words
+		with open("fr.json", "r") as read_file:
+			stop_words_list = json.load(read_file)
+		for word in stop_words_list:
+			[self.result4.remove(element) for element in self.result4 if element == word]
+		self.result5 = self.result4
+		result5 = self.result5
+		return result5
 
 
 def main():
@@ -53,6 +74,9 @@ def main():
 	print("--------------------")
 	test.to_list()
 	print("La liste :\n", test.result4)
+	print("--------------------")
+	test.check_stop_words()
+	print("La liste nettoyée :\n", test.result5)
 
 if __name__ == "__main__":
     main()
--- parser/tests/test_parser01.py
+++ parser/tests/test_parser01.py
@@ -1,8 +1,18 @@
 import unidecode
-from program.parser01 import lowerize
-
+from program.parser01 import *
 
 
 def test_ma_fonction_lowerize_est_ok():
-	result = lowerize()
-	assert result == "bonjour grandpy, j'espère que tu vas bien. dis moi je recherche l'adresse du musée du louvres. sais-tu où cela ce trouve ?"
+	test = Parser()
+	result = test.lowerize()
+	assert result == "bonjour grandpy, j'espère que tu vas bien." \
+	" dis moi je recherche l'adresse du musée du louvres." \
+	" sais-tu où cela ce trouve ?"
+
+def test_ma_fonction_decodize_est_ok():
+	test = Parser()
+	result2 = test.decodize()
+	assert result2 == "bonjour grandpy, j'espere que tu vas bien." \
+	" dis moi je recherche l'adresse du musee du louvres." \
+	" sais-tu ou cela ce trouve ?"
+
